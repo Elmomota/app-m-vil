46c229d08ff0b0d416004ee3edfac7463444596d
Elmomota <163007475+Elmomota@users.noreply.github.com>
2024-08-27 15:18:14 -0400

    node_modules

--- src/app/pages/usuario/forgot-password/forgot-password.page.ts
+++ src/app/pages/usuario/forgot-password/forgot-password.page.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-password.page.scss'],
 })
 export class ForgotPasswordPage implements OnInit {
+  eleCorreo: string="";
 
   constructor() { }
 
